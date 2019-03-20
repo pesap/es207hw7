e799107aa6e15a657514a549d4ae33a3f649c8b9
Jonathan Sam <jonasam95@gmail.com>
2019-03-20 10:06:00 -0700

    Making Functions

--- santa_detect.R
+++ santa_detect.R
@@ -5,3 +5,16 @@ sites_with_san <- loc %>%
   filter(stringr::str_detect(site_name, '^(San|Santa)') )
 sites_with_san
 
+## add so that we can actually count the number of sites
+length(sites_with_san)
+
+# You can also do this as a function, which I believe is what the HW called for
+sites_with_san_detect <- function(x){
+  x %>%
+    filter(stringr::str_detect(site_name, '^(San|Santa)') )
+  b <- length(filter(stringr::str_detect(site_name, '^(San|Santa)') ))
+  assign("NumberOfSites", b)
+  rm(b)
+}
+# example
+# sites_with_san_detect(loc)
